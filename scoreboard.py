3cb31ee3837ba3fff808c5aca46f959057b9f40c
Soumyaranjan Dikshit <105819550+srd1092@users.noreply.github.com>
2023-07-17 15:50:02 +0200

    Add files via upload

---
+++ scoreboard.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Comic Sans MS", 18, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.goto(0, 270)
+        self.display_score()
+
+    def display_score(self):
+        self.clear()
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def update_score(self):
+        self.score += 1
+        self.display_score()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
---
+++ snake.py
@@ -0,0 +1,52 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+HEAD_UP = 90
+HEAD_DOWN = 270
+HEAD_LEFT = 180
+HEAD_RIGHT = 0
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for current_position in STARTING_POSITIONS:
+            self.add_segment(current_position)
+
+    def add_segment(self, position):
+        snake_segment = Turtle("square")
+        snake_segment.color("white")
+        snake_segment.penup()
+        snake_segment.goto(position)
+        self.segments.append(snake_segment)
+
+    def extend_snake(self):
+        self.add_segment(self.segments[-1].position())
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_xcor = self.segments[seg_num - 1].xcor()
+            new_ycor = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_xcor, new_ycor)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != HEAD_DOWN:
+            self.head.setheading(HEAD_UP)
+
+    def down(self):
+        if self.head.heading() != HEAD_UP:
+            self.head.setheading(HEAD_DOWN)
+
+    def left(self):
+        if self.head.heading() != HEAD_RIGHT:
+            self.head.setheading(HEAD_LEFT)
+
+    def right(self):
+        if self.head.heading() != HEAD_LEFT:
+            self.head.setheading(HEAD_RIGHT)
