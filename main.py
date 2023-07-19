39016916ef8df02f2f0390f235ec6b165a281dba
Soumyaranjan Dikshit <105819550+srd1092@users.noreply.github.com>
2023-07-19 10:16:45 +0200

    Update main.py
    Added methods to keep Highscore and reset the game after it ends. Cleaning code.

--- main.py
+++ main.py
@@ -35,7 +35,6 @@ while game_is_on:
 
     # Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        # game_is_on = False
         scoreboard.reset_score()
         snake.reset_snake()
 
@@ -43,7 +42,6 @@ while game_is_on:
     # Detect collision with tail.
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            # game_is_on = False
             scoreboard.reset_score()
             snake.reset_snake()
 
